ef222bbb89f29d3c60f55708446905e3be58c7e1
KKJ <kyujin0911@tukorea.ac.kr>
2024-07-04 21:29:38 +0900

    chore: gradle 재설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,18 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import org.bouncycastle.util.Properties
+import java.io.FileInputStream
+import java.util.*
+
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
     id("dagger.hilt.android.plugin")
     id("androidx.navigation.safeargs.kotlin")
+}
+
+val properties = Properties().apply{
+    load(FileInputStream(rootProject.file("local.properties")))
 }
 
 android {
@@ -23,10 +32,15 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            manifestPlaceholders["NAVER_CLIENT_ID"] = properties["NAVER_CLIENT_ID"] as String
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            isMinifyEnabled = false
+            manifestPlaceholders["NAVER_CLIENT_ID"] = properties["NAVER_CLIENT_ID"] as String
         }
     }
     compileOptions {
@@ -46,14 +60,14 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation ("androidx.lifecycle:lifecycle-runtime:2.6.2")
@@ -93,5 +107,11 @@ dependencies {
     // Coroutines Dependency
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    //naver map
+    implementation("com.naver.maps:map-sdk:3.17.0")
+
+    //위치 추적
+    implementation("com.google.android.gms:play-services-location:21.2.0")
 
 }
--- app/src/main/java/umc/hackathon/presentation/treasure/PreviousTreasureFragment.kt
+++ app/src/main/java/umc/hackathon/presentation/treasure/PreviousTreasureFragment.kt
@@ -1,17 +1,41 @@
 package umc.hackathon.presentation.treasure
 
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.NaverMapOptions
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.widget.ZoomControlView
 import dagger.hilt.android.AndroidEntryPoint
 import umc.hackathon.R
 import umc.hackathon.databinding.FragmentPreviousTreasureBinding
 import umc.hackathon.presentation.base.BaseFragment
 
 @AndroidEntryPoint
-class PreviousTreasureFragment : BaseFragment<FragmentPreviousTreasureBinding>(R.layout.fragment_previous_treasure) {
+class PreviousTreasureFragment : BaseFragment<FragmentPreviousTreasureBinding>(R.layout.fragment_previous_treasure),
+    OnMapReadyCallback {
+    private var naverMap: NaverMap? = null
     override fun initObserver() {
 
     }
 
     override fun initView() {
+        initMap()
+    }
+
+    private fun initMap() {
+        val fm = childFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
+            ?: MapFragment.newInstance(NaverMapOptions().locationButtonEnabled(false)).also {
+                fm.beginTransaction().add(R.id.map_fragment, it).commit()
+            }
+        mapFragment.getMapAsync { map ->
+            naverMap = map
+        }
+    }
+
+    override fun onMapReady(p0: NaverMap) {
 
     }
+
+
 }
